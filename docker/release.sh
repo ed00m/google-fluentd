3502dbdce212c68d993fdeae3d609419eb83b21d
Ling Huang <lingshi@google.com>
2018-08-14 17:09:58 -0400

    Rename variables, use source file, upper case, use internal as default.

--- docker/release.sh
+++ docker/release.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 # Usage:
 # // To build and publish the Docker image to the internal container registry.
-# $ PUBLIC_DESTINATION=internal bash release.sh
+# $ DESTINATION=internal INTERNAL_CONTAINER_REGISTRY=<internal_registry> ./release.sh
 # // To publish the Docker image to the public container registry.
-# $ PUBLIC_DESTINATION=public bash release.sh
+# $ DESTINATION=public ./release.sh
 set -ex
 
-INTERNAL_CONTAINER_REGISTRY='us.gcr.io/container-monitoring-storage/stackdriver-logging-agent'
 PUBLIC_CONTAINER_REGISTRY='gcr.io/stackdriver-agents/stackdriver-logging-agent'
 
-docker_file_version=`grep DOCKER_FILE_VERSION VERSION | sed 's/DOCKER_FILE_VERSION=//g'`
-google_fluentd_version=`grep GOOGLE_FLUENTD_VERSION VERSION | sed 's/GOOGLE_FLUENTD_VERSION=//g'`
+source VERSION
 # TODO(qingling128): Remove this suffix when kubernetes-branch is deprecated.
-image_suffix=""
-docker_image_version="${docker_file_version}-${google_fluentd_version}${image_suffix}"
+IMAGE_SUFFIX=""
+DOCKER_IMAGE_VERSION="${DOCKERFILE_VERSION}-${GOOGLE_FLUENTD_VERSION}${IMAGE_SUFFIX}"
+DESTINATION=${DESTINATION:-internal}
 
-if [[ -z "${PUBLIC_DESTINATION}" ]]; then
-  echo "Please provide a PUBLIC_DESTINATION environment variable.";
+if [[ -z "${INTERNAL_CONTAINER_REGISTRY}" ]]; then
+  echo "Please provide a INTERNAL_CONTAINER_REGISTRY environment variable.";
   exit 1;
 fi
 
-if [ "${PUBLIC_DESTINATION}" == "internal" ]; then
+if [ "${DESTINATION}" == "internal" ]; then
   echo "Building ${DOCKER_IMAGE_VERSION}.";
-  docker build -t "${INTERNAL_CONTAINER_REGISTRY}:${docker_image_version}" .
+  docker build --no-cache -t "${INTERNAL_CONTAINER_REGISTRY}:${DOCKER_IMAGE_VERSION}" .
   echo "Releasing ${DOCKER_IMAGE_VERSION} to the internal container registry.";
-  docker push "${INTERNAL_CONTAINER_REGISTRY}:${docker_image_version}";
-elif [ "${PUBLIC_DESTINATION}" == "public" ]; then
+  docker push "${INTERNAL_CONTAINER_REGISTRY}:${DOCKER_IMAGE_VERSION}";
+elif [ "${DESTINATION}" == "public" ]; then
   echo "Releasing ${DOCKER_IMAGE_VERSION} to the public container registry.";
-  docker pull "${INTERNAL_CONTAINER_REGISTRY}:${docker_image_version}";
-  docker tag "${INTERNAL_CONTAINER_REGISTRY}:${docker_image_version}" "${PUBLIC_CONTAINER_REGISTRY}:${docker_image_version}";
-  docker push "${PUBLIC_CONTAINER_REGISTRY}:${docker_image_version}";
+  docker pull "${INTERNAL_CONTAINER_REGISTRY}:${DOCKER_IMAGE_VERSION}";
+  docker tag "${INTERNAL_CONTAINER_REGISTRY}:${DOCKER_IMAGE_VERSION}" "${PUBLIC_CONTAINER_REGISTRY}:${DOCKER_IMAGE_VERSION}";
+  docker push "${PUBLIC_CONTAINER_REGISTRY}:${DOCKER_IMAGE_VERSION}";
 else
-  echo "Unknown PUBLIC_DESTINATION environment variable ${PUBLIC_DESTINATION}.";
+  echo "Unknown DESTINATION environment variable ${DESTINATION}.";
   exit 1;
 fi
-
