5eaf866db833277a4a6f5b839b70c21f69535cec
Ling Huang <lingshi@google.com>
2018-08-15 14:36:10 -0400

    Add logic to auto detect git branch. Address comments.

--- docker/release.sh
+++ docker/release.sh
@@ -3,16 +3,30 @@
 # // To build and publish the Docker image to the internal container registry.
 # $ DESTINATION=internal INTERNAL_CONTAINER_REGISTRY=<internal_registry> ./release.sh
 # // To publish the Docker image to the public container registry.
-# $ DESTINATION=public ./release.sh
+# $ DESTINATION=public INTERNAL_CONTAINER_REGISTRY=<internal_registry> ./release.sh
 set -ex
 
-PUBLIC_CONTAINER_REGISTRY='gcr.io/stackdriver-agents/stackdriver-logging-agent'
+PUBLIC_CONTAINER_REGISTRY="gcr.io/stackdriver-agents/stackdriver-logging-agent"
+DESTINATION="${DESTINATION:-internal}"
 
+# Set versions. See docker/VERSION file for details.
 source VERSION
-# TODO(qingling128): Remove this suffix when kubernetes-branch is deprecated.
-IMAGE_SUFFIX=""
-DOCKER_IMAGE_VERSION="${DOCKERFILE_VERSION}-${GOOGLE_FLUENTD_VERSION}${IMAGE_SUFFIX}"
-DESTINATION=${DESTINATION:-internal}
+
+# Decide the Docker image version to be released based on the current Git branch.
+GIT_BRANCH=`git status | grep "On branch" | sed 's/On branch //g'`
+case "${GIT_BRANCH}" in
+  lingshi-version)
+    DOCKER_IMAGE_VERSION="${DOCKERFILE_VERSION}-${GOOGLE_FLUENTD_VERSION}";
+    ;;
+  kubernetes)
+    DOCKER_IMAGE_VERSION="${DOCKERFILE_VERSION}-${GOOGLE_FLUENTD_VERSION}-k8s";
+    ;;
+  *)
+    echo "This script should only be used for restricted branches. Exiting.";
+    exit 1;
+    ;;
+esac
+
 
 if [[ -z "${INTERNAL_CONTAINER_REGISTRY}" ]]; then
   echo "Please provide a INTERNAL_CONTAINER_REGISTRY environment variable.";
