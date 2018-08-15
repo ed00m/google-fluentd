be81e51c73ae1c2be3e0d065005d715741937c6f
Ling Huang <lingshi@google.com>
2018-08-15 18:59:24 -0400

    remove internal stuff

--- docker/release.sh
+++ docker/release.sh
@@ -1,55 +1,32 @@
 #!/bin/bash
 # Usage:
-# // To build and publish the Docker image to the internal container registry.
-# $ DESTINATION=internal INTERNAL_CONTAINER_REGISTRY=<internal_registry> ./release.sh
-# // To publish the Docker image to the public container registry.
-# $ DESTINATION=public INTERNAL_CONTAINER_REGISTRY=<internal_registry> ./release.sh
+# // To build and publish the Docker image to a container registry.
+# $ CONTAINER_REGISTRY=<container_registry> ./release.sh
+# // To build and publish the Docker image to a container registry with a certain suffix.
+# $ CONTAINER_REGISTRY=<container_registry> DOCKER_IMAGE_VERSION_SUFFIX=-pre-1 ./release.sh
 set -ex
 
+if [[ -z "${CONTAINER_REGISTRY}" ]]; then
+  echo "Please provide a CONTAINER_REGISTRY environment variable.";
+  exit 1;
+fi
+
 DOCKER_IMAGE_NAME="stackdriver-logging-agent"
-PUBLIC_CONTAINER_REGISTRY="gcr.io/stackdriver-agents"
-DESTINATION="${DESTINATION:-internal}"
 
 # Set version variables. See docker/VERSION file for details.
 source VERSION
 
-# Decide the Docker image version to be released based on the current Git branch.
+DOCKER_IMAGE_VERSION="${DOCKERFILE_VERSION}-${GOOGLE_FLUENTD_VERSION}"
+
+# If not on the master branch, append the branch name as part of the image version to avoid conflicts.
 GIT_BRANCH=`git status | grep "On branch" | sed 's/On branch //g'`
-case "${GIT_BRANCH}" in
-  master)
-    DOCKER_IMAGE_VERSION="${DOCKERFILE_VERSION}-${GOOGLE_FLUENTD_VERSION}";
-    ;;
-  # TODO(qingling128): Remove this case when "kubernetes" branch is deprecated.
-  kubernetes)
-    DOCKER_IMAGE_VERSION="${DOCKERFILE_VERSION}-${GOOGLE_FLUENTD_VERSION}-k8s";
-    ;;
-  *)
-    echo "This script should only be used for restricted branches. Exiting.";
-    exit 1;
-    ;;
-esac
-
-
-if [[ -z "${INTERNAL_CONTAINER_REGISTRY}" ]]; then
-  echo "Please provide a INTERNAL_CONTAINER_REGISTRY environment variable.";
-  exit 1;
+if [ "${GIT_BRANCH}" != "master" ]; then
+  DOCKER_IMAGE_VERSION="${DOCKER_IMAGE_VERSION}-${GIT_BRANCH}";
 fi
 
-case "${DESTINATION}" in
-  internal)
-    echo "Building ${DOCKER_IMAGE_VERSION}.";
-    docker build --no-cache -t "${INTERNAL_CONTAINER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}" .;
-    echo "Releasing ${DOCKER_IMAGE_VERSION} to the internal container registry.";
-    docker push "${INTERNAL_CONTAINER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}";
-    ;;
-  public)
-    echo "Releasing ${DOCKER_IMAGE_VERSION} to the public container registry.";
-    docker pull "${INTERNAL_CONTAINER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}";
-    docker tag "${INTERNAL_CONTAINER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}" "${PUBLIC_CONTAINER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}";
-    docker push "${PUBLIC_CONTAINER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}";
-    ;;
-  *)
-    echo "Unknown DESTINATION environment variable ${DESTINATION}.";
-    exit 1;
-    ;;
-esac
+DOCKER_IMAGE_VERSION="${DOCKER_IMAGE_VERSION}${DOCKER_IMAGE_VERSION_SUFFIX:-}"
+
+echo "Building ${DOCKER_IMAGE_VERSION}."
+docker build --no-cache -t "${CONTAINER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}" .
+echo "Releasing ${DOCKER_IMAGE_VERSION} to the container registry ${CONTAINER_REGISTRY}."
+docker push "${CONTAINER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}"
